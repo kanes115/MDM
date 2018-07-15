f0f17fb479ad6d19cf4aada2129375d646f93e88
kanes115 <dominikstanaszek@icloud.com>
2018-07-15 12:15:37 +0200

    Add machine parser #3

--- mdm/lib/jmmsr_parser.ex
+++ mdm/lib/jmmsr_parser.ex
@@ -33,6 +33,69 @@ defmodule MDM.JmmsrParser do
 
   end
 
+  defmodule MachinesParser do
+
+    @supported_os ["debian", "linux"]
+
+    # TODO Check that ids are unique
+    def check_machines(%{"machines" => machines}) when is_list(machines) do
+      results = for m <- machines, do: {m, check_machine(m)}
+      case take_first_error(results) do
+        :ok -> :ok
+        {_machine, {:error, _} = error} -> error
+      end
+    end
+    def check_machines(%{"machines" => _}), do: {:error, :machines_not_a_list}
+    def check_machines(_), do: {:error, :machines_undefined}
+
+    defp check_machine(machine) do
+      with :ok <- check_name(machine),
+           :ok <- check_id(machine),
+           :ok <- check_address(machine),
+           :ok <- check_os(machine), do: :ok
+    end
+
+    defp check_name(%{"name" => s}) when is_bitstring(s), do: :ok
+    defp check_name(%{"name" => _}), do: {:error, :machine_name_not_string}
+    defp check_name(_), do: :ok
+
+    defp check_id(%{"id" => i}) when is_integer(i), do: :ok
+    defp check_id(%{"id" => _}), do: {:error, :machine_id_not_int}
+    defp check_id(_), do: {:error, :id_undefined}
+
+    defp check_address(%{"ip" => _, "domain" => _}), do: {:error, :ip_and_domain_sepcified}
+    defp check_address(%{"ip" => ip}) do
+      case correct_ip_format?(ip) do
+        true -> :ok
+        false -> {:error, :incorrect_ip_format}
+      end
+    end
+    defp check_address(%{"domain" => d}) do
+      case correct_domain_name_format(d) do
+        true -> :ok
+        false -> {:error, :incorrect_domain_format}
+      end
+    end
+    defp check_address(_), do: {:error, :address_undefined}
+
+    defp check_os(%{"os" => os}) when is_bitstring(os) do
+      case Enum.member?(@supported_os, os) do
+        true -> :ok
+        false -> {:error, :os_not_supported}
+      end
+    end
+    defp check_os(%{"os" => _}), do: {:error, :os_not_string}
+    defp check_os(_), do: {:error, :os_undefined}
+
+    defp correct_ip_format?(_), do: true
+
+    defp correct_domain_name_format(_), do: true
+
+    defp take_first_error([]), do: :ok
+    defp take_first_error([{_machine, :ok} | tail]), do: take_first_error(tail)
+    defp take_first_error([{_machine, {:error, _}} = error | _]), do: error
+  end
+
 
 
   # TODO recursively
@@ -47,7 +110,8 @@ defmodule MDM.JmmsrParser do
   end
 
   defp check_correctness(json) do
-    ConfigParser.check_config(json)
+    :ok = ConfigParser.check_config(json)
+    :ok = MachinesParser.check_machines(json)
   end
 
 end
