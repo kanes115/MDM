cc658d5a713f34c191a8ad78a898433e409add73
Baksztag <jakub.a.kret@gmail.com>
2018-07-15 12:29:38 +0200

    Add redux store provider #16
    Add providers
    Add root reducer
    Add actions stump

---
+++ user_interfaces/gui/mdm-react/src/providers/index.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import {Provider} from 'react-redux';
+
+import store from './store';
+
+import App from '../routes';
+
+class AppProvider extends Component {
+    render() {
+        return (
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        );
+    }
+}
+
+AppProvider.propTypes = {};
+AppProvider.defaultProps = {};
+
+export default AppProvider;
