9ad610c7f5ff50b1fc27833239996961a636f724
Baksztag <jakub.a.kret@gmail.com>
2018-07-17 09:14:46 +0200

    Export CloseIcon component

--- user_interfaces/gui/mdm-react/src/components/index.js
+++ user_interfaces/gui/mdm-react/src/components/index.js
@@ -1,11 +1,13 @@
 import AppContent from './AppContent';
 import AppHeader from './AppHeader';
 import AppNavigation from './AppNavigation';
+import CloseIcon from './CloseIcon';
 import MDMLink from './Link';
 
 export {
     AppContent,
     AppHeader,
     AppNavigation,
+    CloseIcon,
     MDMLink,
 }
