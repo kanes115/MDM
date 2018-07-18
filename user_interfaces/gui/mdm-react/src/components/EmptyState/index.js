9acd55557b4c16a5e7f2872488f91f6903c61a12
Baksztag <jakub.a.kret@gmail.com>
2018-07-18 10:42:16 +0200

    Add EmptyState component

---
+++ user_interfaces/gui/mdm-react/src/components/EmptyState/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './empty-state.css';
+
+const EmptyState = ({children, iconName}) => (
+    <div className="mdm-empty-state">
+        <div className="icon">
+            <i className="material-icons md-64">{iconName}</i>
+        </div>
+        <div className="message">
+            {children}
+        </div>
+    </div>
+);
+
+EmptyState.propTypes = {
+    children: PropTypes.element.isRequired,
+    iconName: PropTypes.string.isRequired,
+};
+EmptyState.defaultProps = {};
+
+export default EmptyState;
