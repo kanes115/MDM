f407da198e893155c974227c2aae4f28de6322fe
kanes115 <dominikstanaszek@icloud.com>
2018-08-12 13:05:39 +0200

    Clean up utils

--- mdm/lib/parser/utils.ex
+++ mdm/lib/parser/utils.ex
@@ -34,73 +34,22 @@ defmodule MDM.JmmsrParser.Utils do
     end)
   end
 
-
-  def if_specified_string(map, field, error_subject) when is_map(map) do
-    case Map.get(map, field, :undefined) do
-      s when is_bitstring(s) -> :ok
-      :undefined -> :ok
-      _ -> {:error, type_error_message(error_subject, "string")}
-    end
-  end
-
-  def if_specified_int(map, field, error_subject) when is_map(map) do
-    case Map.get(map, field, :undefined) do
-      s when is_integer(s) -> :ok
-      :undefined -> :ok
-      _ -> {:error, type_error_message(error_subject, "integer")}
-    end
-  end
-
-
-  def specified_int(map, field, error_subject) when is_map(map) do
-    specified(&is_integer/1, "integer", map, field, error_subject)
-  end
-
-  def specified_string(map, field, error_subject) when is_map(map) do
-    specified(&is_bitstring/1, "string", map, field, error_subject)
-  end
-
-
-  def specified_list(map, field, error_subject) when is_map(map) do
-    specified(&is_list/1, "list", map, field, error_subject)
-  end
-
-  def specified_bool(map, field, error_subject) when is_map(map) do
-    specified(&is_boolean/1, "bool", map, field, error_subject)
-  end
-
-  def specified_path(map, field, error_subject) when is_map(map) do
-    specified(&is_path/1, "path", map, field, error_subject)
-  end
-
-
-  def if_specified_warn(map, field, warning) do
-    case Map.get(map, field, :undefined) do
-      :undefined -> :ok
-      _ ->
-        IO.puts warning
-    end
-  end
-
-  def type_error_message(subject, should_be) do
-    "#{subject} is not a #{should_be}"
-  end
-
-
-  defp specified(type_checker, type, map, field, error_subject) do
-    case Map.get(map, field, :undefined) do
-      :undefined -> {:error, "#{inspect(error_subject)} not specified"}
-      s -> case type_checker.(s) do
-        true -> :ok
-        false -> {:error, type_error_message(error_subject, type)}
-      end
-    end
-  end
-
+  @doc """
+  Checks if given argument is a unix-like path
+  """
   def is_path(s), do: is_bitstring(s) # TODO
 
-  # =======================================================
-  
+  @doc """
+  This function returns values in json specified by
+  path given as 2nd argument. 
+  If the path has lists on its way, it returns a list of all
+  leaf values and puts it inside a tuple `{:list, ListOfActualValues}`
+  to indicate there are multiple values. Each value (or one value if
+  there was no split on list) is put in a tuple `{:value, Value}`.
+
+  ## Examples
+  Example can be seen in test/parser_utils_test.exs
+  """
   def path(json, fields) do
     case do_path(json, fields) do
       v when is_list(v) -> 
@@ -110,6 +59,16 @@ defmodule MDM.JmmsrParser.Utils do
     end
   end
 
+  @doc """
+  Strips the returned by `path/2` function value leaving only
+  the second element of a tuple.
+
+  ## Examples
+  iex(1)> unpack({:list, [value: "ala", value: "ma"]})
+  [value: "ala", value: "ma"]
+  iex(1)> unpack({:value, "ala"})
+  "ala"
+  """
   def unpack({:value, v}), do: v
   def unpack({:list, v}), do: v
   def unpack(list) when is_list(list) do
@@ -136,9 +95,12 @@ defmodule MDM.JmmsrParser.Utils do
     end
   end
 
-  defp print(val, text), do: IO.inspect "#{text} #{inspect(val)}"
-
-  # can't check list values with this!
+  @doc """
+  Checks whether elements under `path` in `json` fulfill
+  given predicate `checker`. If option `must_be_defined?` set to false
+  given path might not exist at all (for some splits on lists) (but if 
+  it exists it must fiulfill the predicate).
+  """
   def check_values(json, path, checker) do
     check_values(json, path, checker, true)
   end
@@ -192,9 +154,6 @@ defmodule MDM.JmmsrParser.Utils do
     end
   end
 
-  defp wrap_in_list(val) when is_list(val), do: val
-  defp wrap_in_list(val), do: [val]
-
   defp maybe_return_error(reason, path) when reason == :not_found or
   reason == :predicate do
     {false, path, reason}
@@ -202,13 +161,12 @@ defmodule MDM.JmmsrParser.Utils do
   defp maybe_return_error(_, _), do: true
 
 
-  def flatten(list, depth \\ -2), do: flatten(list, depth + 1, []) |> Enum.reverse
-  def flatten(list, 0, acc), do: [list | acc]
-  def flatten([h | t], depth, acc) when h == [], do: flatten(t, depth, acc)
-  def flatten([h | t], depth, acc) when is_list(h), do: flatten(t, depth, flatten(h, depth - 1, acc))
-  def flatten([h | t], depth, acc), do: flatten(t, depth, [h | acc])
-  def flatten([], _, acc), do: acc
-
+  defp flatten(list, depth \\ -2), do: flatten(list, depth + 1, []) |> Enum.reverse
+  defp flatten(list, 0, acc), do: [list | acc]
+  defp flatten([h | t], depth, acc) when h == [], do: flatten(t, depth, acc)
+  defp flatten([h | t], depth, acc) when is_list(h), do: flatten(t, depth, flatten(h, depth - 1, acc))
+  defp flatten([h | t], depth, acc), do: flatten(t, depth, [h | acc])
+  defp flatten([], _, acc), do: acc
 
 
   def list_of?(v, pred) when is_list(v) do
@@ -222,7 +180,7 @@ defmodule MDM.JmmsrParser.Utils do
 
   def take_first_error([]), do: true
   def take_first_error([true | tail]), do: take_first_error(tail)
-  def take_first_error([{false, path, reason} | tail]), do: {false, path, reason}
+  def take_first_error([{false, path, reason} | _]), do: {false, path, reason}
 
 
 end
