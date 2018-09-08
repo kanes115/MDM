20839acf1e3cf8b08e23e92f234f04ec7354c419
kanes115 <dominikstanaszek@icloud.com>
2018-09-08 18:14:03 +0200

    Prepare machines to put them to gatherer

--- mdm/lib/parser/jmmsr_parser.ex
+++ mdm/lib/parser/jmmsr_parser.ex
@@ -11,11 +11,16 @@ defmodule MDM.JmmsrParser do
 
   @callback check_typing(Map.t) :: :ok | {false, path, error_desc}
   @callback check_relations(Map.t) :: :ok | {false, path, error_desc}
-    
+
   def from_file(path) do
     with {:ok, body} <- File.read(path),
          {:ok, json} <- Poison.decode(body),
          :ok <- check_correctness(json), do: {:ok, json}
+  end
+
+  def to_internal_repr(json) do
+    json
+    |> keys_to_atoms
   end
 
   def check_correctness(json) do
@@ -63,5 +68,15 @@ defmodule MDM.JmmsrParser do
   defp human_readable_reason(:not_found), do: :not_found
   defp human_readable_reason(:predicate), do: :type_mismatch
   defp human_readable_reason(reason), do: reason
+
+
+  defp keys_to_atoms(string_key_map) when is_map(string_key_map) do
+    for {key, val} <- string_key_map, into: %{}, do: {String.to_atom(key), keys_to_atoms(val)}
+    end
+  defp keys_to_atoms(string_key_map) when is_list(string_key_map) do
+    string_key_map
+    |> Enum.map(&keys_to_atoms/1)
+  end
+  defp keys_to_atoms(value), do: value
 
 end
