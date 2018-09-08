5767ee4d297b308225ee519622461258e46bf19d
kanes115 <dominikstanaszek@icloud.com>
2018-09-08 18:14:03 +0200

    Prepare machines to put them to gatherer

--- mdm/lib/deployer.ex
+++ mdm/lib/deployer.ex
@@ -17,14 +17,16 @@ defmodule MDM.Deployer do
 
   def handle_cast(:wait, _) do
     receive do
-      %Command.Request{command_name: :deploy, body: jmmsr} = req ->
-        case JmmsrParser.check_correctness(jmmsr) do
+      %Command.Request{command_name: :deploy, body: jmmsr0} = req ->
+        case JmmsrParser.check_correctness(jmmsr0) do
           :ok ->
-            # TODO
-            # For now we only answer OK back
-            #
+            jmmsr = jmmsr0 |> JmmsrParser.to_internal_repr
+            machines = jmmsr[:machines]
             # We will maybe sending in body back some diff of jmmsr with machines info.
             # TODO we have to establish some common protocol for DIFFs.
+            #
+            # TODO
+            # For now we only answer OK back
             req
             |> Command.Response.new_answer("ok", 200, %{})
             |> WSCommunicator.send_answer
