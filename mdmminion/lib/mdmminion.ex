c36b206c20030b4db7992f60d56d1a87e22e680c
Jakub Kret <jakub.a.kret@gmail.com>
2018-09-10 21:11:02 +0200

    Initialize mdmminion mix app #39

---
+++ mdmminion/lib/mdmminion.ex
@@ -0,0 +1,18 @@
+defmodule MDMMinion do
+  @moduledoc """
+  Documentation for MDMMinion.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MDMMinion.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
