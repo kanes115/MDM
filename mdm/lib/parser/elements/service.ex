d432b8c8b8ac272399a82b7434d8c4e78421160d
Dominik Stanaszek <dominikstanaszek@icloud.com>
2018-09-27 18:49:25 +0200

    Implement service element

---
+++ mdm/lib/parser/elements/service.ex
@@ -0,0 +1,35 @@
+defmodule MDM.Service do
+  @behaviour MDM.JmmsrElement
+
+  alias MDM.CommonTypes, as: Types
+
+  @type t :: %__MODULE__{
+    name: String.t,
+    service_dir: Types.path(),
+    service_executable: Types.path(),
+    containerized: boolean(),
+    requirements: %{
+      os: [Types.os()],
+      ram: non_neg_integer() | nil,
+      hdd: non_neg_integer() | nil,
+      available_machines: [integer()] # empty means "every machine"
+    }
+  }
+
+  defstruct [:name, :service_dir, :service_executable, :containerized,
+             :requirements]
+
+
+  ## JmmsrElement callbacks
+
+  @spec from_map(map()) :: t()
+  def from_map(map), do: struct(__MODULE__, map)
+
+  @spec to_map(t()) :: map()
+  def to_map(service), do: Map.from_struct(service)
+
+  def key, do: :services
+  def list?, do: true
+
+
+end
