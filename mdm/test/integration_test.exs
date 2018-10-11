196a16b4eb738dd5af4467b02ba0645e3c26a6aa
Dominik Stanaszek <dominikstanaszek@icloud.com>
2018-10-11 17:32:59 +0200

    Restructure websocket handling

--- mdm/test/integration_test.exs
+++ mdm/test/integration_test.exs
@@ -1,9 +1,20 @@
 defmodule IntegrationTests do
   use ExUnit.Case
   import JmmsrHelpers
+
+  # TODO start socket in init_per_suite
+
+  setup do
+    {:ok, pid} = WebSocket.start_link("ws://pilot:8080", self())
+    on_exit fn ->
+      # TODO do it synchronously
+      Process.exit(pid, :kill)
+      :timer.sleep(30)
+    end
+    :ok
+  end
   
   test "Command collect_data returns collected data" do
-    WebSocket.start_link("ws://pilot:8080", self())
     basic_jmmsr()
     |> collect_data
     |> Poison.encode!
@@ -16,11 +27,9 @@ defmodule IntegrationTests do
     2 = length(collected_machines)
     collected_machines
     |> Enum.each(fn m -> check_collected_machine(m, basic_jmmsr()["machines"]) end)
-    WebSockex.send_frame(WebSocket, {:close, "going_away"})
   end
 
   test "Command collect_data can be called multiple times with almost the same result (expect collected resources)" do
-    WebSocket.start_link("ws://pilot:8080", self())
     text = basic_jmmsr()
            |> collect_data
            |> Poison.encode!
@@ -33,8 +42,50 @@ defmodule IntegrationTests do
     m2 = resp2["body"]["collected_data"]
                |> Enum.map(fn %{"machine" => m} -> Map.delete(m, "resources") end)
     assert m1 == m2
-    WebSockex.send_frame(WebSocket, {:close, "going_away"})
   end
+
+  test "On Jmmsr parser error, error is returned in specific format and req can be repeated" do
+    basic_jmmsr()
+    |> JmmsrHelpers.add_to_list(["machines"], machine(
+                                                43, # worng name type
+                                                3232))
+    |> collect_data
+    |> Poison.encode!
+    |> WebSocket.send_text
+    resp = WebSocket.receive() |> Poison.decode!
+    %{
+      "body" => %{"path" => "machines, name", "reason" => "type_mismatch"},
+      "code" => 400,
+      "command_name" => "collect_data",
+      "msg" => "error"
+    } = resp
+    basic_jmmsr()
+    |> collect_data
+    |> Poison.encode!
+    |> WebSocket.send_text
+    resp2 = WebSocket.receive() |> Poison.decode!
+    # TODO extract common part (correct response)
+    200 = resp2["code"]
+    "collect_data" = resp2["command_name"]
+    "collected" = resp2["msg"]
+    collected_machines = resp2["body"]["collected_data"]
+    2 = length(collected_machines)
+    collected_machines
+    |> Enum.each(fn m -> check_collected_machine(m, basic_jmmsr()["machines"]) end)
+  end
+
+  #
+  #  test "Command deploy deploys example services to exactly one machine specified (dump decider)" do
+  #    {:ok, pid} = WebSocket.start_link("ws://pilot:8080", self())
+  #    text = basic_jmmsr()
+  #           |> collect_data
+  #           |> Poison.encode!
+  #    WebSocket.send_text(pid, text)
+  #    deploy()
+  #    |> Poison.encode!
+  #    resp1 = WebSocket.receive(pid) |> Poison.decode! 
+  #    WebSockex.send_frame(pid, {:close, "going_away"})
+  #  end
 
 
 
@@ -44,7 +95,12 @@ defmodule IntegrationTests do
     %{"command_name" => "collect_data",
       "body" => jmmsr}
   end
- 
+
+  defp deploy do
+    %{"command_name" => "deploy",
+      "body" => %{}}
+  end
+  
   def basic_jmmsr do
       %{
         "config" => %{
