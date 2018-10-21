48a96bc5317f26c85815d5920c69867fb55ce838
Dominik Stanaszek <dominikstanaszek@icloud.com>
2018-10-21 12:42:07 +0200

    Fail if pid is not present

--- mdmminion/scripts/linux/service_runner.sh
+++ mdmminion/scripts/linux/service_runner.sh
@@ -6,6 +6,10 @@ COMMAND=$(realpath $1)
 setsid $COMMAND > $(realpath $2) 2>&1 < /dev/null &
 sleep 1
 PID=$(pidof -s $COMMAND)
+if [[ -z $PID ]]; then
+    echo "Service did not start"
+    exit 1
+fi
 PGID=$(ps -p $PID -o pgid= | head -n 1 | tr -d '[:space:]')
 echo "$PGID"
 
