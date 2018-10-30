3a7823026a5e6ba260dbee8890fe4154a717bfa2
Dominik Stanaszek <dominikstanaszek@icloud.com>
2018-10-30 19:39:36 +0100

    Make forker also alloc more memory

--- big_tests/examples/forker_pinger_system/server1/forker.c
+++ big_tests/examples/forker_pinger_system/server1/forker.c
@@ -1,12 +1,19 @@
 #include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv) {
+  
+  srand(time(NULL));
 
   if(fork() == 0) {
-    if(fork() == 0) {
-      while(1) {printf("printing hejoooo");}
+    char* big_space = (char*) malloc(525000000);
+
+    for(int i = 0; i < 525000000; i++)
+      *(big_space+i) = 'a';
+
+    while(1) {
     }
-    while(1) {}
   }
 
 }
